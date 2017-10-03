3498d16ad86a3ab248c81f27afccd7019fe2c1f0
Mihir <mhrpatel12@gmail.com>
2017-10-03 19:25:32 +0530

    Activity for group chat. Integrated firebase for authentication, realtime-database & FCM

--- app/src/main/java/com/suleiman/pagination/activity/MovieDetailsActivity.java
+++ app/src/main/java/com/suleiman/pagination/activity/MovieDetailsActivity.java
@@ -1,9 +1,9 @@
 package com.suleiman.pagination.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -44,8 +44,8 @@ public class MovieDetailsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MovieDetailsActivity.this, GroupChatActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -57,7 +57,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
                 if (response.body() != null) {
                     loadImage(response.body().getPosterPath()).into((ImageView) findViewById(R.id.imgPoster));
                     ((TextView) toolbar.findViewById(R.id.txtTitle)).setText(response.body().getTitle());
-                    ((TextView)findViewById(R.id.txtOverview)).setText(response.body().getOverview());
+                    ((TextView) findViewById(R.id.txtOverview)).setText(response.body().getOverview());
                 }
             }
 
